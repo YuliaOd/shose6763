5feac511eeb5517a9e7e14e7a28985941431b43f
YuliaOd <106181095+YuliaOd@users.noreply.github.com>
2023-01-11 18:49:38 +0000

    Deploying to gh-pages from @ YuliaOd/shose6763@fa86946c4e28013bd9fbb6def7c308453d8ba4d5 🚀

---
+++ depreciation.534f7b70.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}(),$((function(){$("#nav-icon6").click((function(){$(this).toggleClass("open")}))}));
+//# sourceMappingURL=depreciation.534f7b70.js.map
---
+++ depreciation.5ccb5bbc.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})(),$((function(){$("#nav-icon6").click((function(){$(this).toggleClass("open")}))}));
+//# sourceMappingURL=depreciation.5ccb5bbc.js.map
